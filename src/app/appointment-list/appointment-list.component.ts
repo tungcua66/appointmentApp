6788603013e33ba2c49b3baef2f47291fdf9b5b9
tungcua66 <tunghuong66bg@gmail.com>
2025-10-30 15:01:38 +0100

    feat(web): adding logic to display appointments

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -9,9 +9,15 @@ import { Appointment } from '../models/appointment';
 export class AppointmentListComponent {
   newAppointmentTitle: string = ''
   newAppointmentDate: Date = new Date()
-  appointment : Appointment = {
-    id: 1,
-    title: 'Playing with cat',
-    date: new Date('2025-03-06')
-  }
+  appointments : Appointment[] = [];
+   addAppointment = () => {
+    if(this.newAppointmentTitle.trim().length && this.newAppointmentDate){
+      this.appointments.push({
+        id: new Date().getTime(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      });
+      this.newAppointmentTitle = '';
+      this.newAppointmentDate = new Date();
+    }}
 }
