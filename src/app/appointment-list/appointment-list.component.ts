620c51de198c5fe29a0355bfd7180fc1a653bee2
tungcua66 <tunghuong66bg@gmail.com>
2025-11-03 17:21:09 +0100

    chore(web): adding lint and prettier to keep code clean

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -2,22 +2,24 @@ import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
 
 @Component({
-  selector: 'appointment-list',
+  selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
-  styleUrls: ['./appointment-list.component.css']
+  styleUrls: ['./appointment-list.component.css'],
 })
 export class AppointmentListComponent {
-  newAppointmentTitle: string = ''
-  newAppointmentDate: Date = new Date()
-  appointments : Appointment[] = [];
-   addAppointment = () => {
-    if(this.newAppointmentTitle.trim().length && this.newAppointmentDate){
+  newAppointmentTitle = '';
+  newAppointmentDate: Date = new Date();
+  appointments: Appointment[] = [];
+
+  addAppointment() {
+    if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
       this.appointments.push({
         id: new Date().getTime(),
         title: this.newAppointmentTitle,
-        date: this.newAppointmentDate
+        date: this.newAppointmentDate,
       });
       this.newAppointmentTitle = '';
       this.newAppointmentDate = new Date();
-    }}
+    }
+  }
 }
